79396b9941c4b2cb25a98c27327caa8ccc102865
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2020-10-04 23:48:18 +1100

    Added basic helicopter locomotion;
    Helicopters can now perform basic movement and rotation.

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace TowerDefenseGame {
     public class Helicopter : Vehicle
@@ -9,6 +10,7 @@ namespace TowerDefenseGame {
         [SerializeField] float m_Speed;
 
         [Header("Chopper Config")]
+        [SerializeField] LayerMask m_LayerMask;
         [SerializeField] Vector2 m_Tilt;
 
         private Rigidbody m_Rb;
@@ -21,10 +23,19 @@ namespace TowerDefenseGame {
 
         private void Update()
         {
-            ControlCopterMovement();
+            ControlHelicopter();
         }
 
-        private void ControlCopterMovement()
+        private void ControlHelicopter()
+        {
+            MoveHelicopter();
+            RotateHelicopter();
+        }
+
+        /// <summary>
+        /// Move Helicopter on X,Z based on player input.
+        /// </summary>
+        private void MoveHelicopter()
         {
             m_MoveHorizontal = Input.GetAxis("Horizontal");
             m_MoveVertical = Input.GetAxis("Vertical");
@@ -32,9 +43,21 @@ namespace TowerDefenseGame {
             // Tilt player based on velocity
             m_Rb.rotation = Quaternion.Euler(m_MoveVertical * m_Tilt.x, 0, m_MoveHorizontal * -m_Tilt.y);
 
-            Vector3 _input = new Vector3(m_MoveHorizontal, 0, m_MoveVertical).normalized;
+            Vector3 _input = new Vector3(m_MoveHorizontal, 0, m_MoveVertical);
 
             m_Rb.velocity = _input * m_Speed;
+        }
+
+        /// <summary>
+        /// Rotate Helicopter based on mouse position in the world.
+        /// </summary>
+        private void RotateHelicopter()
+        {
+            RaycastHit _hitInfo;
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(_ray, out _hitInfo, Mathf.Infinity, m_LayerMask))
+                m_Rb.transform.LookAt(new Vector3(_hitInfo.point.x, transform.position.y, _hitInfo.point.z));
 
         }
     }
